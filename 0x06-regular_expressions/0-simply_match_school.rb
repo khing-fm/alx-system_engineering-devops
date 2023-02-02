3639a5bad0c55d27a8dcb121844ef4b667664721
Adefemi Adelugba <adefemiade3662@gmail.com>
2023-02-02 09:05:46 +0100

    Ruby

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/env ruby
 # A regular expression that is simply matching School
-puts ARGV[0].scan(/School/).join
+
+require 'oniguruma'
+
+def match(string)
+  regex = Oniguruma::ORegexp.new("School")
+  regex.match(string)
+end
+
+if __FILE__ == $0
+  string = ARGV[0]
+  if match(string)
+    puts "Matched"
+  else
+    puts "Not matched"
+  end
+end
